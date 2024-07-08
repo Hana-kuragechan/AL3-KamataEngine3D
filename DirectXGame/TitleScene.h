3d599eac55f71984970d4e9bc552c5d5de90a1e3
Hana-kuragechan <k023g1049@g.neec.ac.jp>
2024-07-08 16:46:12 +0900

    タイトルシーン追加

---
+++ DirectXGame/TitleScene.h
@@ -0,0 +1,17 @@
+#pragma once
+class TitleScene {
+public:
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+};
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -5,6 +5,7 @@
 #include "ImGuiManager.h"
 #include "PrimitiveDrawer.h"
 #include "TextureManager.h"
+#include"TitleScene.h"
 #include "WinApp.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -17,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	AxisIndicator* axisIndicator = nullptr;
 	PrimitiveDrawer* primitiveDrawer = nullptr;
 	GameScene* gameScene = nullptr;
-
+	TitleScene* titleScene = nullptr;
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
 	win->CreateGameWindow(L"GC2B_10_タムラ_ハナコ_AL3");
